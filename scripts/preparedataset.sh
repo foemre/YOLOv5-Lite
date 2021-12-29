ee181083fa5ec65e62c6c9c0242725e0a30b02d5
Emre Erdem <fo.emre@gmail.com>
2021-12-29 11:07:39 +0300

    bugfixes

--- scripts/preparedataset.sh
+++ scripts/preparedataset.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 wget -P ../../ http://assets.laboro.ai/laborotomato/laboro_tomato.zip
 cd ../../
 unzip laboro_tomato.zip
-cp YOLOv5-Lite/convert_json2yolo.py laboro_tomato
+cp YOLOv5-Lite/scripts/convert_json2yolo.py laboro_tomato
 mkdir -p laboro_tomato/images/
 cp -r laboro_tomato/train laboro_tomato/images/train
 cp -r laboro_tomato/test laboro_tomato/images/test
